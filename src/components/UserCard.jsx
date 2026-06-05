5b60029fbaa84a817cbff8496cd7c3236ca24be2
aroamartindelarubia-coder <aroamartindelarubia@gmail.com>
2026-06-05 21:36:09 +0200

    añado tailwind a las tarjetas

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,18 +1,31 @@
-import './UserCard.css'
-
+import linkedinLogo from '../assets/linkedin.png'
 function UserCard(props) {
-  return (
-    <div className='tarjeta'>
-        <img src={props.imagen} alt="Foto de perfil" className='tarjeta-img'/>
-        <div className='tarjeta-info'>
-            <h3>{props.nombre}</h3>
-            <p className='profesion'>
-                {props.profesion}
-            </p>
-            <button className='btn-conectar'>Conectar</button>
-        </div>
-    </div>
-  )
-}
+  return(
+  //MAGIA TAILWIND
+  <div className="bg-white w-64 rounded-xl shadow-lg 
+  overflow-hidden hover:shadow-2x1 hover:-translate-y-2 transition-all
+  duration-300 brightness-50">
+    <img
+      src={props.imagen}
+      alt="Foto de Perfil"
+      className="w-full h-48 object-cover"
+    />
+    <div className="p-6 text-center">
+      <h3 className="text-xl font-bold text-gray-800">{props.nombre}</h3>
+      <p className="text-sm text-gray-500 my-3 flex items-center
+      justify-center gap-2">
+        {props.profesion}
+        <img src={linkedinLogo} alt='Linkedin' className='w-5 h-5'>
+      </img>
+      </p>
 
-export default UserCard
+      <button className="bg-blue-600 text-white px-6 py-2 rounded-full
+      font-bold hover:bg-blue-700 transition-colors">
+        Conectar
+      </button>
+
+    </div>
+  </div>
+  );
+}
+export default UserCard;
